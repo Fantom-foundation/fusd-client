5c3ca118430ecab77c64cd839f47fec07ca62f69
coding-pangolin256 <fssolution@outlook.com>
2021-08-30 22:54:52 +1000

    Added utils for dealing with address

---
+++ src/utils/index.js
@@ -0,0 +1,33 @@
+import { ethers } from 'ethers';
+import { getAddress } from '@ethersproject/address';
+
+export function isAddress(value) {
+  try {
+    return getAddress(value);
+  } catch {
+    return false;
+  }
+}
+
+export function shortenAddress(address, chars = 4) {
+  if (!address) return '';
+
+  const parsed = isAddress(address);
+  if (!parsed) {
+    throw Error(`Invalid 'address' parameter '${address}'.`);
+  }
+  return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`;
+}
+
+export const formatNumber = num => {
+  if (isNaN(num)) return '';
+  let parts = num.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
+};
+
+export const calculateGasMargin = value => {
+  return value
+    .mul(ethers.BigNumber.from(10000).add(ethers.BigNumber.from(1000)))
+    .div(ethers.BigNumber.from(10000));
+};
