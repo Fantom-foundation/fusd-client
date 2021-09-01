204a72a662e4f190c343cad2c04a6b72fd7e2e55
coding-pangolin256 <fssolution@outlook.com>
2021-09-01 20:52:45 +1000

    Added wallet connectors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
+import Web3ReactManager from './components/Web3ReactManager';
 import reportWebVitals from './reportWebVitals';
 import store from './stores/reduxStore';
-import { Provider } from 'react-redux';
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+const NetworkContextName = 'NETWORK';
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Web3ProviderNetwork getLibrary={getLibrary}>
+          <Web3ReactManager>
+            <App />
+          </Web3ReactManager>
+        </Web3ProviderNetwork>
+      </Web3ReactProvider>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
