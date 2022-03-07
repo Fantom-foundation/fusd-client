291f9d6c05a098c9a223fd73484ce27527ea70a9
coding-pangolin256 <fssolution@outlook.com>
2022-03-08 00:00:03 +1000

    Updated env usage

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 export const urls = {
-	api_url: process.env.API_URL || 'http://localhost:4000',
-	auction_api_url: process.env.AUCTION_API_URL || 'http://localhost:5001',
+	api_url: process.env.REACT_APP_API_URL || 'http://localhost:4000',
+	auction_api_url: process.env.REACT_APP_AUCTION_API_URL || 'http://localhost:5001',
 	pricefeed: 'https://oapi.fantom.network/pricefeed/fantom'
 } 
