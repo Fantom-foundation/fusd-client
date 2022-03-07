1ba8356ed60a7cf31873ed148f9110ed70b398f7
coding-pangolin256 <fssolution@outlook.com>
2022-03-07 23:52:55 +1000

    Updated API integration

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 export const urls = {
-	api_url: 'http://localhost:4000',
-	auction_api_url: 'http://localhost:5001',
+	api_url: process.env.API_URL || 'http://localhost:4000',
+	auction_api_url: process.env.AUCTION_API_URL || 'http://localhost:5001',
 	pricefeed: 'https://oapi.fantom.network/pricefeed/fantom'
 } 
